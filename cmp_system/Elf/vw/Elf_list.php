60e8a0705aba7c2753736db5de96659621a7e491
vadimgreat <kellapmorgan1@gmail.com>
2015-02-22 13:58:05 +0200

    a

--- cmp_system/Elf/vw/Elf_list.php
+++ cmp_system/Elf/vw/Elf_list.php
@@ -8,8 +8,11 @@
 	<?php foreach($elfs as $elf){ ?>
 	      <div>
 		 <?php
-	     $this->Input('textarea', 'elf_elf', 'name', $elf['id_elf'], $elf['name'], Array(),$elf["id_owner"] );
+	     $this->Input('text', 'elf_elf', 'name', $elf['id_elf'], $elf['name'], Array(),$elf["id_owner"] );
 	     $this->Input('checkmark', 'elf_elf', 'is_visible', $elf['id_elf'], $elf['is_visible'], Array(),$elf["id_owner"] );
+		 $this->Input('textarea', 'elf_elf', 'description', $elf['id_elf'], $elf['name'], Array(),$elf["id_owner"]);
+		 $this->Input('combobox', 'elf_elf', 'color', $elf['id_elf'], $elf['color'], Array(),$elf["id_owner"]);
+		 $this->Input('date', 'elf_elf', 'data', $elf['id_elf'], $elf['data'], Array(),$elf["id_owner"]);
 		 $this->loadToolBox("elf",$elf['id_elf'],null,null,null,array('delete'), $view_update_link);
 		 ?>
 		 </div>
