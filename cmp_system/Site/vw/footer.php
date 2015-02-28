47748359951227ffe41a96ed11c32a1e7b015a4c
unknown <gromovyy@gmail.com>
2015-02-28 12:27:09 +0200

    commit

--- cmp_system/Site/vw/footer.php
+++ cmp_system/Site/vw/footer.php
@@ -1,2 +1,48 @@
-    </body>
+<footer>
+			<div class="footer col-xs-12">
+				<div class="div11 col-md-3 col-xs-6">
+					<h3>Навігація</h3>
+					<ul>
+						<li><a href="#">Проекти</a></li>
+						<li><a href="#">Про нас</a></li>
+						<li><a href="#">Новини</a></li>
+					</ul>
+						
+					
+				</div>
+				<div class="div22 col-md-3 col-xs-6">
+					<h3>Наш фонд</h3>
+					<p>
+						БО "БФ "КОМБАТ-ЮА"<br>
+						04073 м. Київ,<br>
+						вул. Скляренка, 1 <br>
+						ЄДРПОУ: 39547270 <br>
+						Р/р 26006052736644 <br>
+						в ПАТ КБ ПриватБанк, <br>
+						м. Київ <br>
+						МФО: 300711 
+					</p>
+				</div>
+				<div class="div33 col-md-3 col-xs-6">
+					<h3>Контакти</h3>
+					<p>
+						Контактна інформація <br>
+						Номер телефону <br>
+						+38 000 000 00 00 <br>
+						Email: info@qqqqqqq.com
+					</p>
+				</div>
+				<div class="div44 col-md-3 col-xs-6">
+					<h3>Підтримати нас</h3>
+					<p>
+						<img src="img/footer.png" alt="" class="col-xs-12">
+					</p>
+				</div>
+			</div>
+		</footer>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>	
+</body>
+
 </html>
--- cmp_system/Site/vw/header.php
+++ cmp_system/Site/vw/header.php
@@ -1,33 +1,64 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-		<title>Tracker : IT-Factory</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-		<meta name="robots" content="noindex, nofollow"/>
-		<link rel="shortcut icon" href="favicon.ico">
-		<?php
-		$this->includeCSS("bootstrap.min.css");
-		$this->includeCSS("bootstrap-theme.min.css");
-		$this->includeCSS("bootstrapValidator.min.css");
-		$this->includeCSS("style.css");
-		$this->includeCSS("font-awesome.min.css");
-		
-		$this->includeJS("bootstrap.js");
-		$this->includeJS("bootstrapValidator.min.js");
-		$this->includeJS("ru_RU.js");
-		$this->includeJS("jquery.loadJSON.js");
-		$this->includeJS("jquery-ui-timepicker-addon.js");
-		$this->includeJS("jquery.cookie.js");
-		$this->includeJS('//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.2/ckeditor.js');
-		$this->loadCSS();
-		$this->loadJS();
-		$this->loadJsData();
-		?>
-    </head>
-    <body class="<?php echo($this->User->getMode()=="edit")?"active-edit-mode":"active-view-mode";?>"> 
-		<?php // echo "Роль=".$this->User->getRole();?>
-    	<div class="background-shy-image"></div>
-		<?php
-					$this->loadPosition("menu", "workLayout");
-					//$this->loadPosition("authorization", "workLayout");
-    	?>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<div class="container">
+		<header>
+		<div class="menu col-xs-12 col-sm-12 col-md-12">
+			<div class="row">
+				<div class="div1 col-xs-12 col-sm-6 col-md-4">
+					<ul>
+						<li><a href="#">Укр.</a></li>
+						<li><a href="#">Eng.</a></li>
+						<li><a href="#">Рус.</a></li>		
+					</ul>
+					<h1>Combat-UA</h1>
+					<h4>Фонд сприяння полкам спецназу</h4>
+				</div>
+				<div class="div2 col-sm-6 col-md-4">
+					<img src="img/header/u19.jpg" alt="">
+				</div>
+				<div class="div3  col-md-4">
+					<ul>
+						<li><a href="#"><img src="img/header/social/u23.png" alt=""></a></li>
+						<li><a href="#"><img src="img/header/social/u25.png" alt=""></a></li>
+						<li><a href="#"><img src="img/header/social/u27.png" alt=""></a></li>
+						<li><a href="#"><img src="img/header/social/u29.png" alt=""></a></li>
+					</ul>
+					<p><a href="">Новини</a> | <a href="">Про нас</a></p>
+				</div>
+			</div>
+			<div class="row">
+				<nav class="navbar navbar-default col-sm-12 col-md-9">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+					    <div class="navbar-header">
+					      	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navi">
+					        <span class="sr-only">Toggle navigation</span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					      </button>
+
+					    </div>
+					    <div class="collapse navbar-collapse" id="navi">
+					    	<ul class="nav navbar-nav">
+					    		<li><a href="#">Усі проекти</a></li>
+					    		<li><a href="#">Озброєння</a></li>
+					    		<li><a href="#">Медицина</a></li>
+					    		<li><a href="#">Амуніція</a></li>
+					    		<li><a href="#">Технічні засоби</a></li>
+					    	</ul>
+					    </div>
+					</div>
+				</nav>
+			</div>
+		</div>
+		</header>
