aad094dbe31fcb4f210ae4d843e635889a5f4da1
unknown <gromovyy@gmail.com>
2015-03-01 10:51:49 +0200

    commit

--- cmp_system/Site/vw/footer.php
+++ cmp_system/Site/vw/footer.php
@@ -41,8 +41,6 @@
 			</div>
 		</footer>
 	</div>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="bootstrap/js/bootstrap.min.js"></script>	
 </body>
 
 </html>
--- cmp_system/Site/vw/header.php
+++ cmp_system/Site/vw/header.php
@@ -3,10 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Document</title>
-
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/main.css">
+	<title>Combat-UA</title>
+	<?php $this->includeCSS("bootstrap.min.css"); ?>
+	<?php $this->includeCSS("main.css"); ?>
+	<?php $this->includeJS("jquery-1.11.2.min.js"); ?>
+	<?php $this->includeJS("bootstrap.js"); ?>
 </head>
 <body>
 	<div class="container">
